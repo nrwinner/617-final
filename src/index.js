33b97a4e0188e562ed25c6cbbfe6f72389db6cc8
Nick Winner <design@winnerdigital.net>
2018-03-21 23:37:01 -0400

    Configure application to use Babel, SCSS, Flow, and probably other things I forgot

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+let root = document.getElementById('root');
+if (root) {
+    ReactDOM.render(<App />, root);
+    registerServiceWorker();
+} else {
+    // error handling
+    console.error('Where\'s your root element bruh?');
+}
